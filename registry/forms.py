c750efccb2b15378a8a7e1a3141b809c25595248
Richard Pope <richard@memespring.co.uk>
2015-01-19 13:09:47 +0000

    gov styles

--- registry/forms.py
+++ registry/forms.py
@@ -3,7 +3,7 @@ from wtforms import Form, TextField, PasswordField, RadioField, validators
 class LoginForm(Form):
     email = TextField('Email', [validators.Required(message="Enter a valid email"), validators.Email(message="Enter a valid email")])
     password = PasswordField('Password', [validators.Required(message="Enter your password")])
-    twofactor = TextField('Security key', [validators.Required(message="Use your security key")])
+    twofactor = TextField('Security key <i class="fa fa-key"></i>', [validators.Required(message="Use your security key")])
 
 class ChooseProviderForm(Form):
     provider = RadioField('Choose provider', choices=[('vouch', 'Vouch me'), ('idat', 'IDAT')])
