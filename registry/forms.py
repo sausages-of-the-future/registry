b8d1b947f4db8e65c553dc3a86b1841a29c424d0
Richard Pope <richard@memespring.co.uk>
2015-01-27 13:08:44 +0000

    email field

--- registry/forms.py
+++ registry/forms.py
@@ -1,7 +1,8 @@
 from wtforms import Form, TextField, PasswordField, RadioField, validators
+from wtforms.fields import html5
 
 class LoginForm(Form):
-    email = TextField('Email', [validators.Required(message="Enter a valid email"), validators.Email(message="Enter a valid email")])
+    email = html5.EmailField('Email', [validators.Required(message="Enter a valid email"), validators.Email(message="Enter a valid email")])
     password = PasswordField('Password', [validators.Required(message="Enter your password")])
     twofactor = TextField('Security key <i class="fa fa-key"></i>', [validators.Required(message="Use your security key")])
 
