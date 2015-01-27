ab868ca5aea836d23e3ed0fd0e905f4d97f1cfdb
Adam Shimali <adam.shimali@gmail.com>
2015-01-27 16:46:21 +0000

    better exception logging

--- registry/decorators.py
+++ registry/decorators.py
@@ -1,8 +1,17 @@
+import traceback
+
 from datetime import datetime
 from functools import wraps
 from flask import session, request, redirect, url_for, current_app
 
 from registry.auth import AuthUserLog
+
+def log_traceback(logger, ex, ex_traceback=None):
+    if ex_traceback is None:
+        ex_traceback = ex.__traceback__
+    tb_lines = [ line.rstrip('\n') for line in
+                 traceback.format_exception(ex.__class__, ex, ex_traceback)]
+    logger.info(tb_lines)
 
 def audit_log(f):
     @wraps(f)
@@ -15,8 +24,9 @@ def audit_log(f):
             user_log.occured_at = datetime.now()
             user_log.client = request.oauth.client
             user_log.save()
-        except RuntimeError as e:
-            current_app.logger.info('error %s' % e)
+        except Exception as ex:
+            log_traceback(current_app.logger, ex)
+
         return f(*args, **kwargs)
     return decorated_function
 
