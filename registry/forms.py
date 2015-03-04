6f1012b137e66a08ed04215b958f0e5882fcf437
stephen mccarthy <stephen.mccarthy@digital.cabinet-office.gov.uk>
2015-03-04 15:19:26 +0000

    updated design of choose provider and permission pages

--- registry/forms.py
+++ registry/forms.py
@@ -4,7 +4,7 @@ from wtforms.fields import html5
 class LoginForm(Form):
     email = html5.EmailField('Email', [validators.Required(message="Enter a valid email"), validators.Email(message="Enter a valid email")])
     password = PasswordField('Password', [validators.Required(message="Enter your password")])
-    twofactor = TextField('Security key <i class="fa fa-key"></i>', [validators.Required(message="Use your security key")])
+    twofactor = html5.IntegerField('Security key <i class="fa fa-key"></i>', [validators.Required(message="Use your security key")])
 
 class ChooseProviderForm(Form):
     provider = RadioField('Choose provider', choices=[('vouch', 'Vouch me'), ('idat', 'IDAT')])
